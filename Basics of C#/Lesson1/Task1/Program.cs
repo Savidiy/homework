e11a5179fa40bc5251543aa84c69ddcae75d932f
Savidiy <savidiy@mail.ru>
2020-12-06 19:11:52 +0300

    start

---
+++ Basics of C#/Lesson1/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Васильев Леонид
+//
+//1.Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку.
+//а) используя склеивание;
+//б) используя форматированный вывод;
+//в) *используя вывод со знаком $.
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

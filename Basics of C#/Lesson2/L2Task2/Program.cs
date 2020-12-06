36a00223fd4a29b4ba51dea7a4471a259be93f52
Savidiy <savidiy@mail.ru>
2020-12-06 21:47:58 +0300

    task 2 start

---
+++ Basics of C#/Lesson2/L2Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Васильев Леонид
+//Урок 2. Задание 2.Написать метод подсчета количества цифр числа.
+
+namespace L2Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+    }
+}

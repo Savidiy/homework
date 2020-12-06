97aac050248e3e4307eba668db56df9cdab52ee1
Savidiy <savidiy@mail.ru>
2020-12-06 19:46:08 +0300

    task 1 done

--- Basics of C#/Lesson1/Task1/Program.cs
+++ Basics of C#/Lesson1/Task1/Program.cs
@@ -5,18 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 
 //Васильев Леонид
-//
-//1.Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку.
-//а) используя склеивание;
-//б) используя форматированный вывод;
-//в) *используя вывод со знаком $.
 
 namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+
+            #region Task 1
+            //1.Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку.
+            //а) используя склеивание;
+            //б) используя форматированный вывод;
+            //в) *используя вывод со знаком $.
+
+            Console.WriteLine("=Анкета=");
+            Console.Write("Введите ваше имя:\n>");
+            string name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию:\n>");
+            string lastname = Console.ReadLine();
+            Console.Write("Введите ваш возраст:\n>");
+            string age = Console.ReadLine();
+            Console.Write("Введите ваш рост в сантиметрах:\n>");
+            string heigth = Console.ReadLine();
+            Console.Write("Введите ваш вес в килограммах:\n>");
+            string weigth = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Вас зовут " + name + " " + lastname + ".");
+            Console.Write(" Ваш возраст {0}.", age);
+            Console.WriteLine($" Ваш рост {heigth} см. Ваш вес {weigth} кг.");
+            Console.ReadKey();
+            #endregion
 
         }
     }

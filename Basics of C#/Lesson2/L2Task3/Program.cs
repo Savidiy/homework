8698a9bf9d164d62a862722220a8013b4aef4ab5
Savidiy <savidiy@mail.ru>
2020-12-06 22:19:32 +0300

    task 3 is solved

--- Basics of C#/Lesson2/L2Task3/Program.cs
+++ Basics of C#/Lesson2/L2Task3/Program.cs
@@ -10,7 +10,30 @@ namespace L2Task3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("= Задание 3: Сумма введенных нечетных положительных чисел =");
 
+           
+            Console.WriteLine("\n Вводите целые числа (0 для остановки):");
+
+            int sum = 0;
+            int k = 2;
+            do
+            {
+                Console.Write(">");
+                k = Convert.ToInt32(Console.ReadLine());
+                if (k > 0)
+                {
+                    if (k % 2 == 1)
+                    {
+                        sum += k;
+                    }
+                }
+                Console.WriteLine($" Сумма введенных нечетных положительны чисел равна {sum}.");
+            }
+            while (k != 0);
+
+            Console.Write("\nНажмите любую клавишу");
+            Console.ReadKey();
         }
     }
 }

f503c6efcbab1b5be9a51f1d49cfa85db6935097
Savidiy <savidiy@mail.ru>
2020-12-06 22:06:37 +0300

    task 3 start

---
+++ Basics of C#/Lesson2/L2Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+//Васильев Леонид
+//3.С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+
+namespace L2Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

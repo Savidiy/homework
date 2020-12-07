b00b80445156ba7fee7c523360cab3423927c56b
Savidiy <savidiy@mail.ru>
2020-12-07 10:02:59 +0300

    task 6 variant 1

--- Basics of C#/Lesson2/L2Task6/Program.cs
+++ Basics of C#/Lesson2/L2Task6/Program.cs
@@ -20,12 +20,20 @@ namespace L2Task6
             #region Расчеты
             int count = 0;
             int procent = -1;
-            int max = 1000000000;
+            int start = 1;
+            int max = 10000000;//1000000000
             int border = 0;
 
-            for (var i = 1; i <= max; i++)
+            IsGoodNumber(100);
+            IsGoodNumber(220);
+            IsGoodNumber(1);
+            IsGoodNumber(0);
+
+            for (var i = start; i <= max; i++)
             {
                 //15 sec without logic in IsGoodNumber
+                //count 61 574 510 with use only isGoodNumber 5 min 3 sec
+                //
                 if (IsGoodNumber(i))
                 {
                     count++;
@@ -44,14 +52,38 @@ namespace L2Task6
             DateTime endTime = DateTime.Now;
             TimeSpan delta = endTime.Subtract(startTime);
 
+            Console.WriteLine($"Количество хороших чисел {count}.");
             Console.WriteLine($"Расчеты заняли {delta.Minutes:d2}:{delta.Seconds:d2}.{delta.Milliseconds:d3} ({delta.Ticks} тиков).");
             Console.Write("Нажмите любую клавишу");
             Console.ReadKey();
         }
+                
+        /// <summary>
+        /// Хорошим называется число, которое делится на сумму своих цифр. 
+        /// </summary>
+        /// <param name="k"></param>
+        /// <returns></returns>
         static bool IsGoodNumber(int k)
         {
+            k = Math.Abs(k);
+            if (k == 0) 
+                return false;
+            long digitSum = 0;
+            long divider = 1;
+            long remains = k;
 
-            return true;
+            while (divider <= k)
+            {
+                long mod = remains / divider - (remains / (divider * 10 )) * 10;
+                if (mod > 0)
+                {
+                    digitSum += mod;
+                }
+                remains -= mod * divider;
+                divider *= 10;
+            }            
+
+            return k % digitSum == 0;
         }
     }
 

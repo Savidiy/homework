d5de05b608b7c52bc607acd20bc6a0d6a2dec3fb
Savidiy <savidiy@mail.ru>
2020-12-07 11:15:28 +0300

    task 7 start

---
+++ Basics of C#/Lesson2/L2Task7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Васильев Леонид
+// 7.a) Разработать рекурсивный метод, который выводит на экран числа от a до b (a<b);
+// б) *Разработать рекурсивный метод, который считает сумму чисел от a до b.
+
+namespace L2Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

7b02ac2221f962c87f0f91a68acfbb8ac40b29ae
Savidiy <savidiy@mail.ru>
2020-12-07 09:33:09 +0300

    task 6 prepare without calculation

--- Basics of C#/Lesson2/L2Task6/Program.cs
+++ Basics of C#/Lesson2/L2Task6/Program.cs
@@ -14,7 +14,45 @@ namespace L2Task6
     {
         static void Main(string[] args)
         {
+            DateTime startTime = DateTime.Now;
 
+            Console.WriteLine("Начинаю подсчет количества «Хороших» чисел в диапазоне от 1 до 1 000 000 000.");
+            #region Расчеты
+            int count = 0;
+            int procent = -1;
+            int max = 1000000000;
+            int border = 0;
+
+            for (var i = 1; i <= max; i++)
+            {
+                //15 sec without logic in IsGoodNumber
+                if (IsGoodNumber(i))
+                {
+                    count++;
+                }
+                //progree bar does not take many time ~300 mills
+                if (i > border)
+                {
+                    procent++;
+                    border = max / 100 * procent;
+                    Console.SetCursorPosition(0, 1);
+                    Console.WriteLine($"{procent}%");
+                }
+            }
+            #endregion
+
+            DateTime endTime = DateTime.Now;
+            TimeSpan delta = endTime.Subtract(startTime);
+
+            Console.WriteLine($"Расчеты заняли {delta.Minutes:d2}:{delta.Seconds:d2}.{delta.Milliseconds:d3} ({delta.Ticks} тиков).");
+            Console.Write("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+        static bool IsGoodNumber(int k)
+        {
+
+            return true;
         }
     }
+
 }

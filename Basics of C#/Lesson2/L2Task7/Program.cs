b177d6f4348dec85d9488eece6e108e21ba14e4b
Savidiy <savidiy@mail.ru>
2020-12-07 11:27:40 +0300

    task 7 part A

--- Basics of C#/Lesson2/L2Task7/Program.cs
+++ Basics of C#/Lesson2/L2Task7/Program.cs
@@ -15,6 +15,66 @@ namespace L2Task7
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write("  Введите целое число А\n> ");
+                int a = StringToIntSafe(Console.ReadLine());
+                Console.Write("  Введите целое число B\n> ");
+                int b = StringToIntSafe(Console.ReadLine());
+
+                if (a > b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+
+                Console.WriteLine("\n Последовательный вывод:");
+                RepeatOutput(a, b);
+
+
+                Console.WriteLine("\nНажмите любую клавишу для повторения\n");
+                Console.ReadKey();
+                //Console.Clear();
+            }
+        }
+
+        static void RepeatOutput(int a, int b)
+        {
+            Console.Write($"{a} ");
+            if (a < b)
+                RepeatOutput(a + 1, b);
+        }
+
+        static int StringToIntSafe(string t, bool onlyPositive = false)
+        {
+            string res = "";
+
+            foreach (var c in t)
+            {
+                if (char.IsDigit(c))
+                {
+                    res += c;
+                }
+            }
+
+            if (res.Length > 0)
+            {
+                //there are digits, chech negative number
+                if (!onlyPositive)
+                {
+                    if (t[0] == '-')
+                        res = "-" + res;
+                }
+            }
+            else
+            {
+                //trere are no digits
+                return 0;
+            }
+
+            return Convert.ToInt32(res);
         }
     }
 }

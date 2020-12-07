0ec19ff6192ec7923789eb96487bac142d109943
Savidiy <savidiy@mail.ru>
2020-12-07 08:47:36 +0300

    task 6 start

---
+++ Basics of C#/Lesson2/L2Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Васильев Леонид
+// 6. * Написать программу подсчета количества «Хороших» чисел в диапазоне от 1 до 1 000 000 000. 
+// Хорошим называется число, которое делится на сумму своих цифр. 
+// Реализовать подсчет времени выполнения программы, используя структуру DateTime.
+namespace L2Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

67c94d80d2d9f32826c1fe9e231a2ff24c62f672
Savidiy <savidiy@mail.ru>
2020-12-11 17:51:34 +0300

    start lesson 3 task 1

---
+++ Basics of C#/Lesson3/L3Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Леонид Васильев
+// 1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры;
+// б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
+
+namespace L3Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

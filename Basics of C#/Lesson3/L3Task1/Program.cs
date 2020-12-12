4a1ff2bb0d30ed1d66243401512f81537add0896
Savidiy <savidiy@mail.ru>
2020-12-12 10:34:19 +0300

    typo and style fix

--- Basics of C#/Lesson3/L3Task1/Program.cs
+++ Basics of C#/Lesson3/L3Task1/Program.cs
@@ -10,136 +10,136 @@ using System.Threading.Tasks;
 
 namespace L3Task1
 {
-    class Program
+    struct ComplexStr
     {
-        struct ComplexStr
+        public double _re;
+        public double _im;
+
+        public ComplexStr(double re, double im)
         {
-            public double _re;
-            public double _im;
+            _re = re;
+            _im = im;
+        }
 
-            public ComplexStr(double re, double im)
-            {
-                _re = re;
-                _im = im;
-            }
+        public ComplexStr Plus (ComplexStr a)
+        {
+            ComplexStr r;
+            r._re = _re + a._re;
+            r._im = _im + a._im;
+            return r;
+        }
+        public ComplexStr Minus (ComplexStr a)
+        {
+            ComplexStr r;
+            r._re = _re - a._re;
+            r._im = _im - a._im;
+            return r;
+        }
+        public ComplexStr Multi (ComplexStr a)
+        {
+            ComplexStr r;
+            r._re = _re * a._re - _im * a._im;
+            r._im = _re * a._im + _im * a._re;
+            return r;
+        }
 
-            public ComplexStr Plus (ComplexStr a)
+        public override string ToString()
+        {
+            string t = "";
+            if (_re != 0)
             {
-                ComplexStr r;
-                r._re = _re + a._re;
-                r._im = _im + a._im;
-                return r;
-            }
-            public ComplexStr Minus (ComplexStr a)
+                t += _re;
+            } 
+            else
             {
-                ComplexStr r;
-                r._re = _re - a._re;
-                r._im = _im - a._im;
-                return r;
-            }
-            public ComplexStr Multi (ComplexStr a)
-            {
-                ComplexStr r;
-                r._re = _re * a._re - _im * a._im;
-                r._im = _re * a._im + _im * a._re;
-                return r;
-            }
-
-            public override string ToString()
-            {
-                string t = "";
-                if (_re != 0)
+                if (_im != 0)
                 {
-                    t += _re;
+                    return $"{_im}i";
                 } 
                 else
                 {
-                    if (_im != 0)
-                    {
-                        return $"{_im}i";
-                    } 
-                    else
-                    {
-                        return "0";
-                    }                        
-                }
-
-                if (_im != 0)
-                {
-                    t += (_im > 0) ? $"+{_im}i" : $"-{-_im}i";
-                }
-
-                return t;
+                    return "0";
+                }                        
             }
-        }
 
-        class ComplexCl
+            if (_im != 0)
+            {
+                t += (_im > 0) ? $"+{_im}i" : $"-{-_im}i";
+            }
+
+            return t;
+        }
+    }
+
+    class ComplexCl
+    {
+        private double _re;
+        private double _im;
+
+        public double Re
         {
-            private double _re;
-            private double _im;
-
-            public double Re
-            {
-                get { return _re; }
-                set { _re = value; }
-            }
-            public double Im
-            {
-                get { return _im; }
-                set { _im = value; }
-            }
-
-            public ComplexCl()
-            {
-                _re = _im = 0;
-            }
-            public ComplexCl(double re, double im)
-            {
-                _re = re;
-                _im = im;
-            }
-
-            public ComplexCl Plus(ComplexCl a)
-            {                
-                return new ComplexCl(_re + a._re, _im + a._im);
-            }
-            public ComplexCl Minus(ComplexCl a)
-            {
-                return new ComplexCl(_re - a._re, _im - a._im);
-            }
-            public ComplexCl Multi(ComplexCl a)
-            {
-                return new ComplexCl(_re * a._re - _im * a._im, _re * a._im + _im * a._re);
-            }
-
-            public override string ToString()
-            {
-                string t = "";
-                if (_re != 0) // ex.: 7...
-                {
-                    t += _re;
-                }
-                else
-                {
-                    if (_im != 0) // ex.: 5i or -5i
-                    {
-                        return $"{_im}i";
-                    }
-                    else // ex.: 0
-                    {
-                        return "0";
-                    }
-                }
-
-                if (_im != 0) // ex.: 7+5i or 7-5i
-                {
-                    t += (_im > 0) ? $"+{_im}i" : $"{_im}i";
-                }
-
-                return t;
-            }
+            get { return _re; }
+            set { _re = value; }
+        }
+        public double Im
+        {
+            get { return _im; }
+            set { _im = value; }
         }
 
+        public ComplexCl()
+        {
+            _re = _im = 0;
+        }
+        public ComplexCl(double re, double im)
+        {
+            _re = re;
+            _im = im;
+        }
+
+        public ComplexCl Plus(ComplexCl a)
+        {                
+            return new ComplexCl(_re + a._re, _im + a._im);
+        }
+        public ComplexCl Minus(ComplexCl a)
+        {
+            return new ComplexCl(_re - a._re, _im - a._im);
+        }
+        public ComplexCl Multi(ComplexCl a)
+        {
+            return new ComplexCl(_re * a._re - _im * a._im, _re * a._im + _im * a._re);
+        }
+
+        public override string ToString()
+        {
+            string t = "";
+            if (_re != 0) // ex.: 7...
+            {
+                t += _re;
+            }
+            else
+            {
+                if (_im != 0) // ex.: 5i or -5i
+                {
+                    return $"{_im}i";
+                }
+                else // ex.: 0
+                {
+                    return "0";
+                }
+            }
+
+            if (_im != 0) // ex.: 7+5i or 7-5i
+            {
+                t += (_im > 0) ? $"+{_im}i" : $"{_im}i";
+            }
+
+            return t;
+        }
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
             while (true)
@@ -162,21 +162,24 @@ namespace L3Task1
                 ComplexCl complexClass2 = new ComplexCl(re2, im2);
 
                 Console.WriteLine("");
-                Console.WriteLine("Вы задали два комплексных чилса.");
+                Console.WriteLine("Вы задали два комплексных числа.");
                 Console.WriteLine("Выведем их на экран:");
                 Console.WriteLine($"Классы:\t\t{complexClass1}\t\t{complexClass2}");
                 Console.WriteLine($"Структуры:\t{complexStruct1}\t\t{complexStruct2}");
-                Console.WriteLine("Сумма этих чисел:");
+
+                Console.WriteLine("\nСумма этих чисел:");
                 Console.WriteLine($"Классы:\t\t{complexClass1.Plus(complexClass2)}");
                 Console.WriteLine($"Структуры:\t{complexStruct1.Plus(complexStruct2)}");
-                Console.WriteLine("Разность этих чисел:");
+
+                Console.WriteLine("\nРазность этих чисел:");
                 Console.WriteLine($"Классы:\t\t{complexClass1.Minus(complexClass2)}");
                 Console.WriteLine($"Структуры:\t{complexStruct1.Minus(complexStruct2)}");
-                Console.WriteLine("Произведение этих чисел:");
+
+                Console.WriteLine("\nПроизведение этих чисел:");
                 Console.WriteLine($"Классы:\t\t{complexClass1.Multi(complexClass2)}");
                 Console.WriteLine($"Структуры:\t{complexStruct1.Multi(complexStruct2)}");
 
-                Console.WriteLine("\nДля ввода новой пары чисел нажмите любую клавишу");
+                Console.Write("\nДля ввода новой пары чисел нажмите любую клавишу");
                 Console.ReadKey();                
             }
         }

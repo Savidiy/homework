a0821ba106d620c81919a578b940ae4911f42da0
Savidiy <savidiy@mail.ru>
2020-12-13 12:50:32 +0300

    lesson 4 task 1 is solved

--- Basics of C#/Lesson4/L4Task1/Program.cs
+++ Basics of C#/Lesson4/L4Task1/Program.cs
@@ -17,8 +17,41 @@ namespace L4Task1
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                int min = -10000;
+                int max = 10000;
+                Random random = new Random();
+                int[] nums = new int[20];
+                Console.Write("\nДан массив чисел:\n");
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    nums[i] = random.Next(min, max);
+                    if (nums[i] % 3 == 0)
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    else
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write($"{nums[i]} ");
+                }
 
+                int counter = 0;
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if ((nums[i] % 3 == 0) || (nums[i - 1] % 3 == 0))
+                    {
+                        counter++;
+                    }
+                }
 
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write($"\n\nЗдесь есть ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(counter);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine($" пар соседних чисел, где хотя бы одно число делится на 3.");
+                Console.WriteLine("Нажмите любую клавишу для повтора");
+                Console.ReadKey();
+            }
         }
     }
 }

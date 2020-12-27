7ca791882ad50a143ed274fae5197cebb557c6a0
Savidiy <savidiy@mail.ru>
2020-12-27 22:45:47 +0300

    L6 T1 start

---
+++ Basics of C#/Lesson6/L6Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// Леонид Васильев
+/// 1. Изменить программу вывода функции так, чтобы можно было передавать функции типа double (double,double). 
+/// Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+
+namespace L6Task1
+{
+    public delegate double Fun(double x);
+
+    class Program
+    {
+        public static void Table(Fun F, double x, double b)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
+        // Создаем метод для передачи его в качестве параметра в Table
+        public static double MyFunc(double x)
+        {
+            return x * x * x;
+        }
+
+        static void Main(string[] args)
+        {
+            // Создаем новый делегат и передаем ссылку на него в метод Table
+            Console.WriteLine("Таблица функции MyFunc:");
+            // Параметры метода и тип возвращаемого значения, должны совпадать с делегатом
+            Table(new Fun(MyFunc), -2, 2);
+            Console.WriteLine("Еще раз та же таблица, но вызов организован по новому");
+            // Упрощение(c C# 2.0).Делегат создается автоматически.            
+            Table(MyFunc, -2, 2);
+            Console.WriteLine("Таблица функции Sin:");
+            Table(Math.Sin, -2, 2);      // Можно передавать уже созданные методы
+            Console.WriteLine("Таблица функции x^2:");
+            // Упрощение(с C# 2.0). Использование анонимного метода
+            Table(delegate (double x) { return x * x; }, 0, 3);
+
+            Console.Write("Нажмите любую клавишу для выхода");
+            Console.ReadKey(true);
+        }
+    }
+}

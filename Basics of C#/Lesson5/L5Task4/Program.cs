d3292888fec0abff7e3e6a3f68ac807e054ea644
Savidiy <savidiy@mail.ru>
2020-12-27 14:10:42 +0300

    L5 T4 is solved

--- Basics of C#/Lesson5/L5Task4/Program.cs
+++ Basics of C#/Lesson5/L5Task4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,121 @@ using System.Threading.Tasks;
 
 namespace L5Task4
 {
+    class Student
+    {
+        public float AverageScore { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Student (string firstName, string lastName, int s1, int s2, int s3)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            AverageScore = (s1 + s2 + s3) / 3f;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Clear();
+                string filename = "students1.txt";
+                // string filename = "students2.txt"; // всего два варианта среднего бала
+                PrintLn($"Будут загружены данные из файла {filename}");
+                Print("Нажмите Enter для продолжения или другую клавишу для ввода имени файла.");
+                var key = Console.ReadKey(true);
+                PrintLn();
+                if (key.Key != ConsoleKey.Enter)
+                {
+                    PrintLn("Введите имя файла:");
+                    filename = Console.ReadLine();
+                }
 
+                string path = AppDomain.CurrentDomain.BaseDirectory + filename;
+                PrintLn($"Загружаю данные из файла {filename}");
+                if (File.Exists(path))
+                {
+                    #region Загрузка данных из файла
+                    int count = 0;
+                    Student[] students;
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        count = int.Parse(sr.ReadLine());
+                        students = new Student[count];
+                        for (int i = 0; i < count; i++)
+                        {
+                            string line = sr.ReadLine();
+                            var data = line.Split(' ');
+                            string lastName = data[0];
+                            string firstName = data[1];
+                            int s1 = int.Parse(data[2]);
+                            int s2 = int.Parse(data[3]);
+                            int s3 = int.Parse(data[4]);
+                            students[i] = new Student(firstName, lastName, s1, s2, s3);
+                        }
+                    }
+                    PrintLn($"Загружено {count} строк.");
+                    #endregion
+
+                    #region Ищем минимальные средние
+                    float upLimit = 6;
+                    float mins1 = upLimit;
+                    float mins2 = upLimit;
+                    float mins3 = upLimit;
+                    for (int i = 0; i < count; i++)
+                    {
+                        float aver = students[i].AverageScore;
+                        if (aver < mins3)
+                        {
+                            if (aver < mins2)
+                            {
+                                if (aver < mins1)
+                                {
+                                    mins3 = mins2;
+                                    mins2 = mins1;
+                                    mins1 = aver;
+                                }
+                                else if (aver != mins1)
+                                { 
+                                    mins3 = mins2;
+                                    mins2 = aver;
+                                }
+                            }
+                            else if (aver != mins2)
+                            {
+                                mins3 = aver;
+                            }
+                        }
+                    }
+                    // коррекция на случай если у всех одинаковый минимальный балл
+                    if (mins2 == upLimit) mins2 = mins1;
+                    if (mins3 == upLimit) mins3 = mins2;
+                    #endregion
+
+                    #region Выводим результаты
+                    PrintLn("Три ученика с минимальными средними баллами (больше трех при равенстве баллов):");
+                    for (int i = 0; i < count; i++)
+                    {
+                        float aver = students[i].AverageScore;
+                        if (aver <= mins3)
+                        {
+                            PrintLn($"{students[i].LastName} {students[i].FirstName} {aver:f2}");
+                        }
+                    }
+                    #endregion
+                }
+                else
+                {
+                    PrintLn($"Не найден файл {path}");
+                }
+                PrintLn();
+                PrintLn($"Нажмите любую клавишу для повтора");
+                Console.ReadKey(true);
+
+            }
         }
         static public void PrintLnWithColor(string text, ConsoleColor color)
         {

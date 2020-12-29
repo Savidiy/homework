70ed7810d50ac4712a58ca6d156c514dd707a390
Savidiy <savidiy@mail.ru>
2020-12-29 19:47:06 +0300

    L7 T2 partial solved

--- Basics of C#/Lesson7/L7Task2/Form1.Designer.cs
+++ Basics of C#/Lesson7/L7Task2/Form1.Designer.cs
@@ -30,20 +30,22 @@ namespace L7Task2
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pbGirl = new System.Windows.Forms.PictureBox();
+            this.lblDialog = new System.Windows.Forms.Label();
+            this.tbHistory = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.MenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.NewGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.btnAnswer = new System.Windows.Forms.Button();
-            this.lblDialog = new System.Windows.Forms.Label();
-            this.pbGirl = new System.Windows.Forms.PictureBox();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGirl)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbGirl)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -53,21 +55,47 @@ namespace L7Task2
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.Controls.Add(this.pbGirl);
             this.panel1.Controls.Add(this.lblDialog);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.tbHistory);
             this.panel1.Location = new System.Drawing.Point(0, 27);
             this.panel1.Name = "panel1";
-            this.panel1.Padding = new System.Windows.Forms.Padding(3, 3, 150, 3);
-            this.panel1.Size = new System.Drawing.Size(464, 283);
+            this.panel1.Padding = new System.Windows.Forms.Padding(3, 3, 180, 3);
+            this.panel1.Size = new System.Drawing.Size(685, 283);
             this.panel1.TabIndex = 2;
             // 
-            // textBox1
+            // pbGirl
             // 
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(3, 3);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(311, 277);
-            this.textBox1.TabIndex = 0;
+            this.pbGirl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbGirl.Image = global::L7Task2.Properties.Resources._4;
+            this.pbGirl.Location = new System.Drawing.Point(531, 6);
+            this.pbGirl.Name = "pbGirl";
+            this.pbGirl.Size = new System.Drawing.Size(132, 125);
+            this.pbGirl.TabIndex = 2;
+            this.pbGirl.TabStop = false;
+            // 
+            // lblDialog
+            // 
+            this.lblDialog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDialog.Location = new System.Drawing.Point(511, 134);
+            this.lblDialog.Margin = new System.Windows.Forms.Padding(0);
+            this.lblDialog.Name = "lblDialog";
+            this.lblDialog.Size = new System.Drawing.Size(168, 145);
+            this.lblDialog.TabIndex = 1;
+            this.lblDialog.Text = "label1";
+            this.lblDialog.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbHistory
+            // 
+            this.tbHistory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbHistory.Location = new System.Drawing.Point(3, 3);
+            this.tbHistory.Multiline = true;
+            this.tbHistory.Name = "tbHistory";
+            this.tbHistory.ReadOnly = true;
+            this.tbHistory.Size = new System.Drawing.Size(502, 277);
+            this.tbHistory.TabIndex = 0;
+            this.tbHistory.TabStop = false;
+            this.tbHistory.Text = "фыв\r\nфывфыв\r\nфывы\r\n";
             // 
             // menuStrip1
             // 
@@ -75,7 +103,7 @@ namespace L7Task2
             this.MenuToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(464, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(685, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -91,87 +119,97 @@ namespace L7Task2
             // NewGameToolStripMenuItem
             // 
             this.NewGameToolStripMenuItem.Name = "NewGameToolStripMenuItem";
-            this.NewGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.NewGameToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.NewGameToolStripMenuItem.Text = "Новая игра";
+            this.NewGameToolStripMenuItem.Click += new System.EventHandler(this.NewGameToolStripMenuItem_Click);
             // 
             // ExitToolStripMenuItem
             // 
             this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
-            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.ExitToolStripMenuItem.Text = "Выход";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
             // 
             // panel2
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnNewGame);
             this.panel2.Controls.Add(this.btnAnswer);
             this.panel2.Controls.Add(this.tbNumber);
             this.panel2.Location = new System.Drawing.Point(0, 309);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(464, 57);
+            this.panel2.Size = new System.Drawing.Size(685, 57);
             this.panel2.TabIndex = 4;
             // 
-            // tbNumber
+            // label1
             // 
-            this.tbNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.tbNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.tbNumber.Location = new System.Drawing.Point(112, 14);
-            this.tbNumber.Name = "tbNumber";
-            this.tbNumber.Size = new System.Drawing.Size(110, 26);
-            this.tbNumber.TabIndex = 0;
-            this.tbNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(167, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ваше число";
             // 
             // btnAnswer
             // 
             this.btnAnswer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.btnAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnAnswer.Location = new System.Drawing.Point(228, 13);
+            this.btnAnswer.Location = new System.Drawing.Point(398, 12);
             this.btnAnswer.Name = "btnAnswer";
             this.btnAnswer.Size = new System.Drawing.Size(120, 28);
             this.btnAnswer.TabIndex = 1;
             this.btnAnswer.Text = "Ответить";
             this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
             // 
-            // lblDialog
+            // tbNumber
             // 
-            this.lblDialog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblDialog.AutoSize = true;
-            this.lblDialog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblDialog.Location = new System.Drawing.Point(355, 184);
-            this.lblDialog.Name = "lblDialog";
-            this.lblDialog.Size = new System.Drawing.Size(53, 20);
-            this.lblDialog.TabIndex = 1;
-            this.lblDialog.Text = "label1";
+            this.tbNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.tbNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbNumber.Location = new System.Drawing.Point(282, 13);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(110, 26);
+            this.tbNumber.TabIndex = 0;
+            this.tbNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbNumber.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbNumber_KeyDown);
             // 
-            // pbGirl
+            // btnNewGame
             // 
-            this.pbGirl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.pbGirl.Location = new System.Drawing.Point(320, 6);
-            this.pbGirl.Name = "pbGirl";
-            this.pbGirl.Size = new System.Drawing.Size(132, 125);
-            this.pbGirl.TabIndex = 2;
-            this.pbGirl.TabStop = false;
+            this.btnNewGame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnNewGame.Location = new System.Drawing.Point(272, 12);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(120, 28);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "Новая игра";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Visible = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 361);
+            this.ClientSize = new System.Drawing.Size(685, 361);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.panel1);
             this.MinimumSize = new System.Drawing.Size(480, 400);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Угадай число";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGirl)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbGirl)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,7 +218,7 @@ namespace L7Task2
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbHistory;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem MenuToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem NewGameToolStripMenuItem;
@@ -190,6 +228,8 @@ namespace L7Task2
         private System.Windows.Forms.TextBox tbNumber;
         private System.Windows.Forms.PictureBox pbGirl;
         private System.Windows.Forms.Label lblDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNewGame;
     }
 }
 
--- Basics of C#/Lesson7/L7Task2/Form1.cs
+++ Basics of C#/Lesson7/L7Task2/Form1.cs
@@ -12,25 +12,215 @@ namespace L7Task2
 {
     public partial class Form1 : Form
     {
-        /// Помоги мне отгадать число. Оно точно больше и меньше.
-        /// Точно, забыла сказать, что это целое число.
-        /// Хорошее число, но слишком большое.
-        /// Маловато!
-        /// Добавь еще, тебе что жалко?
-        /// Надо больше...
-        /// Поменьше бы...
-        /// Похоже, но должно быть меньше.
-        /// Блин... этот вариант слишком маленький.
-        /// Мне кажется, что нужное число больше.
+
+        int minValue = 1;
+        int maxValue = 100;
+        int countOfAttempt;
+        int maxAttemt;
+        int magicNumber;
+        Random random;
 
         public Form1()
         {
             InitializeComponent();
+            random = new Random();
+            NewGame();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        void NewGame()
         {
+            magicNumber = random.Next(minValue, maxValue + 1);
+            maxAttemt = (int)Math.Ceiling(Math.Log(maxValue - minValue + 2, 2)); // 7 значений 3 попытки, 8 значения 4 попытки
+            string text = $"Я загадала число от {minValue} до {maxValue}. Попробуй его угадать с {maxAttemt} попыт{GetWordEndByNumber("ки","ок","ок",maxAttemt)}.";
 
+            tbHistory.Clear();
+            tbNumber.Visible = true;
+            btnAnswer.Visible = true;
+            label1.Visible = true;
+            btnNewGame.Visible = false;
+            tbNumber.Focus();
+            countOfAttempt = 0;
+            
+            SetPic(GirlPicEnum.Hello);
+            GirlSay(text);
+        }
+
+        enum GirlPicEnum { Hello, GameOver, Error, Win, MyNumIsLess, MyNumIsBigger}
+        void SetPic(GirlPicEnum num)
+        {
+            switch (num)
+            {
+                case GirlPicEnum.MyNumIsLess:
+                case GirlPicEnum.MyNumIsBigger:
+                    switch (random.Next(0, 6))
+                    {
+                        case 0: pbGirl.Image = L7Task2.Properties.Resources._1; break;
+                        case 1: pbGirl.Image = L7Task2.Properties.Resources._6; break;
+                        case 2: pbGirl.Image = L7Task2.Properties.Resources._2; break;
+                        case 3: pbGirl.Image = L7Task2.Properties.Resources._3; break;
+                        case 4: pbGirl.Image = L7Task2.Properties.Resources._4; break;
+                        case 5: pbGirl.Image = L7Task2.Properties.Resources._2; break;
+                    }
+                    break;
+                case GirlPicEnum.Error:
+                    switch (random.Next(0, 4))
+                    {
+                        case 0: pbGirl.Image = L7Task2.Properties.Resources._4; break;
+                        case 1: pbGirl.Image = L7Task2.Properties.Resources._6; break;
+                        case 2: pbGirl.Image = L7Task2.Properties.Resources._2; break;
+                        case 3: pbGirl.Image = L7Task2.Properties.Resources._5; break;
+                    }
+                    break;
+                case GirlPicEnum.GameOver:
+                    pbGirl.Image = L7Task2.Properties.Resources.gameover;
+                    break;
+                case GirlPicEnum.Win:
+                    switch (random.Next(0, 2))
+                    {
+                        case 0: pbGirl.Image = L7Task2.Properties.Resources._1; break;
+                        case 1: pbGirl.Image = L7Task2.Properties.Resources._6; break;
+                    }
+                    break;
+                case GirlPicEnum.Hello:
+                    switch (random.Next(0, 2))
+                    {
+                        case 0: pbGirl.Image = L7Task2.Properties.Resources._4; break;
+                        case 1: pbGirl.Image = L7Task2.Properties.Resources._6; break;
+                    }
+                    break;
+            }
+
+        }
+
+        void GirlSay(string text, int num = 0)
+        {
+            lblDialog.Text = $"\"{text}\"";
+            string prefix = num == 0 ? "" : num + ". ";
+            tbHistory.Text += $"{prefix}{text}\r\n";
+        }
+
+        void GetAnswer()
+        {
+            string text = tbNumber.Text;
+            tbNumber.Text = "";
+            int num;
+            if (text == "")
+            {
+                SetPic(GirlPicEnum.Error);
+                GirlSay($"Ты думаешь я загадала ничего? Нет, число самое всамделишное!");
+                // И что это? Мнимая единица?
+                // Если ты имел ввиду 0, то я ебя не поняла.
+            }
+            else if (int.TryParse(text, out num))
+            {
+                if (num < minValue)
+                {
+                    SetPic(GirlPicEnum.Error);
+                    GirlSay($"Ты меня вообще слушал? Число должно быть больше {minValue - 1}.");
+                }
+                else if (num > maxValue)
+                {
+                    SetPic(GirlPicEnum.Error);
+                    GirlSay($"С кем я разговариваю! Число должно быть не больше {maxValue}.");
+                }
+                else
+                {
+                    countOfAttempt++;
+                    if (num == magicNumber)
+                    {
+                        SetPic(GirlPicEnum.Win);
+                        GirlSay($"Вау! Я и правда загадала {num}. Ты что мысли читаешь?", countOfAttempt);
+                        // скрываем текстовое поле
+                        tbNumber.Visible = false;
+                        btnAnswer.Visible = false;
+                        label1.Visible = false;
+                        btnNewGame.Visible = true;
+                        btnNewGame.Focus();
+                    }
+                    else
+                    {
+                        if (num > magicNumber)
+                        {
+                            SetPic(GirlPicEnum.MyNumIsLess);
+                            GirlSay($"Я бы такое не загадала! {num} это же большое число!", countOfAttempt);
+                        }
+                        else
+                        {
+                            SetPic(GirlPicEnum.MyNumIsBigger);
+                            GirlSay($"Ха! {num}! Вот это мелочь ты придумал!", countOfAttempt);
+                        }
+                        if (countOfAttempt >= maxAttemt)
+                        {
+                            SetPic(GirlPicEnum.GameOver);
+                            GirlSay($"У тебя закончились попытки. Повезет в другой раз.");
+                            // скрываем текстовое поле
+                            tbNumber.Visible = false;
+                            btnAnswer.Visible = false;
+                            label1.Visible = false;
+                            btnNewGame.Visible = true;
+                            btnNewGame.Focus();
+                        }
+                    }
+                }
+            }
+            else if (double.TryParse(text, out double dd))
+            {
+                SetPic(GirlPicEnum.Error);
+                GirlSay("Точно! Забыла сказать, что это целое число.");
+            }
+            else
+            {
+                SetPic(GirlPicEnum.Error);
+                GirlSay("Ты точно знаешь как выглядят цифры?");
+            }            
+        }
+
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            GetAnswer();
+        }
+
+        private void NewGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void tbNumber_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                GetAnswer();
+            }
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+        public static string GetWordEndByNumber(string s1, string s4, string s5, int num)
+        {
+            num %= 100;
+            if (num >= 11 && num <= 19)
+                return s5;
+            else
+            {
+                num %= 10;
+                switch (num)
+                {
+                    case 1: return s1;
+                    case 2:
+                    case 3:
+                    case 4: return s4;
+                    default: return s5;
+                }
+            }
         }
     }
 }

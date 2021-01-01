8b1b7f7736a9382abd52465fb50f613867314ec0
Savidiy <savidiy@mail.ru>
2021-01-01 19:55:29 +0300

    L8 T3 start

---
+++ Basics of C#/Lesson8/L8Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// Леонид Васильев
+/// 3. *Написать программу-преобразователь из CSV в XML-файл с информацией о студентах (6 урок).
+
+namespace L8Task3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CSVtoXMLconverterForm());
+        }
+    }
+}
